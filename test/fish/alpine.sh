b92afb166e0fda9e7a56555156771818d0cf9663
eitsupi <ts1s1andn@gmail.com>
2024-01-04 17:53:28 +0900

    🧪 test scripts can be run by non-root user

--- test/fish/alpine.sh
+++ test/fish/alpine.sh
@@ -2,24 +2,11 @@
 
 set -e
 
-NON_ROOT_USER=""
-POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
-for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
-  if id -u ${CURRENT_USER} >/dev/null 2>&1; then
-    NON_ROOT_USER=${CURRENT_USER}
-    break
-  fi
-done
-if [ "${NON_ROOT_USER}" = "" ]; then
-  NON_ROOT_USER=root
-fi
-
 # Optional: Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
 check "fish" fish -v
-echo "Testing with user: ${NON_ROOT_USER}"
-check "fisher" su "${NON_ROOT_USER}" -c 'fish -c "fisher -v"'
+check "fisher" fish -c "fisher -v"
 
 # Report result
 reportResults
